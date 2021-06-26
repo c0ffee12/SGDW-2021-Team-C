b646fc0f9ca28320bb1bae3df4253639986a5673
Sean Mei <smei0528@gmail.com>
2021-06-26 13:29:27 -0700

    Small bug fix
    test push

--- SJSU-GDW-2021-Team-C/Assets/Scripts/Enemies/RabidMovement.cs
+++ SJSU-GDW-2021-Team-C/Assets/Scripts/Enemies/RabidMovement.cs
@@ -6,6 +6,8 @@ public class RabidMovement : MonoBehaviour
 {
     public float speed;
     public float playerDetectDistance;     //length of ray for mouse to detect floor 
+    public float floorDetectRayDistance;
+    private bool movingRight;
     public Transform groundDetect;
     public Transform player;
 
@@ -14,21 +16,44 @@ public class RabidMovement : MonoBehaviour
         float distToPlayer = Vector2.Distance(transform.position, player.position);
         if(distToPlayer < playerDetectDistance)
         {
-            chasePlayer();
+            speed = 4;
+            ChasePlayer();
         }
-            //mouse will stop if player not detected
+        else
+        {
+            speed = 1;
+            Patrol();    
+        }
     }
-    private void chasePlayer()
+    private void ChasePlayer()
     {
-        if (transform.position.x < player.position.x)
+        if (transform.position.x < player.position.x + 0.2f)
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
-        else
+        else   
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
             transform.eulerAngles = new Vector3(0, -180, 0);
+        }
+    }
+    private void Patrol()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundCheck = Physics2D.Raycast(groundDetect.position, Vector2.down, floorDetectRayDistance);
+        if (groundCheck.collider == false)
+        {
+            if (movingRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
         }
     }
 }
