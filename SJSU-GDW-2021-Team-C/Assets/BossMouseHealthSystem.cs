1f14c9d6ff94e5d0e245a7bf07bf3bbdcc9d114c
Sean Mei <smei0528@gmail.com>
2021-07-29 00:36:34 -0700

    Added a boss mouse
    Boss mouse is a variant of rabid mouse that has health and additional states

--- SJSU-GDW-2021-Team-C/Assets/BossMouseHealthSystem.cs
+++ SJSU-GDW-2021-Team-C/Assets/BossMouseHealthSystem.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class BossMouseHealthSystem : MonoBehaviour
 {
-    public int heatlh;
+    CatFSM catFSM;
+    BossMovement BossMouse;
+    public bool canTakeDamage;
+    public int Bosshealth;
 
+    private void Start()
+    {
+        canTakeDamage = true;
+        BossMouse = GameObject.FindGameObjectWithTag("BossMouse").GetComponent<BossMovement>();
+        catFSM = GameObject.FindGameObjectWithTag("Player").GetComponent<CatFSM>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("SpringTailHitBox") && !(catFSM.curState is PlayerTakeDamageState) && (BossMouse.speed <= 2))
+        {
+            Bosshealth -= 1;
+            if (Bosshealth <= 0)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            if(Bosshealth == 5 || Bosshealth == 2)
+            {
+                BossMouse.PanicMode();
+                Debug.Log("PANIC MODE ACTIVATED");
+            }
+        }
+    }
 }
